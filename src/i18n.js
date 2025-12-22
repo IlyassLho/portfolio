0b3190c37b5abf60c681ca259862bdef157775d2
IlyassLho <ilyasslhouari@gmail.com>
2025-12-22 23:30:01 +0100

    Add: i18n configuration and language resources for internationalization support

---
+++ src/i18n.js
@@ -0,0 +1,30 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+
+// Import translations
+import en from "./locales/en.json";
+import ar from "./locales/ar.json";
+import fr from "./locales/fr.json";
+import de from "./locales/de.json";
+import ru from "./locales/ru.json";
+
+// Initialize i18n
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: { ...en },
+      ar: { ...ar },
+      fr: { ...fr },
+      de: { ...de },
+      ru: { ...ru },
+    },
+    fallbackLng: "en",
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n;
